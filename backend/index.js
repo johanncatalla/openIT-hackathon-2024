f0d98327f56ba921c7a79808eb8dec72a2d2a47a
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2024-06-01 14:15:32 +0800

    backend-setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+mongoose.connect('mongodb+srv://OpenCSame:PfvEiZAwoBI5eM3c@opencsame.frnarkw.mongodb.net/?retryWrites=true&w=majority&appName=OpenCSame')
+    .then(() => {
+        console.log('Connected to MongoDB')
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000')
+        })
+    })
+    .catch((e) => {
+        console.log('Connection failed', e)
+    })
