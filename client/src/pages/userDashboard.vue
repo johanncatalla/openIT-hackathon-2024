49755fcfed46fbad0845a13e927f7dcf0ce60a4c
Johann Sebastian Catalla <catallajohann@gmail.com>
2024-06-01 19:51:32 +0800

    dashboard

---
+++ client/src/pages/userDashboard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ul class="tab-menu">
+            <li :class="{ active: activeTab === 'dashboard' }" @click="activeTab = 'dashboard'">Dashboard</li>
+        </ul>
+
+        <div v-if="activeTab === 'dashboard'">
+            
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeTab: 'dashboard'
+        };
+    }
+};
+</script>
+
+<style>
+.tab-menu {
+    list-style-type: none;
+    display: flex;
+    justify-content: left;
+    padding: 0;
+}
+
+.tab-menu li {
+    cursor: pointer;
+    padding: 10px;
+}
+
+.tab-menu li.active {
+    background-color: #ccc;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -11,6 +11,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import index from '@/pages/index.vue'
 import adminDashboard from '@/pages/adminDashboard.vue'
+import userDashboard from '@/pages/userDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -24,6 +25,12 @@ const router = createRouter({
       path: '/adminDashboard/:accessToken',
       name: 'adminDashboard',
       component: adminDashboard,
+      props: true
+    },
+    {
+      path: '/userDashboard/:accessToken',
+      name: 'userDashboard',
+      component: userDashboard,
       props: true
     }
   ]
