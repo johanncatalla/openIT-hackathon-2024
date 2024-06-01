f6f44ce8a61b0285a21d849da6f3662ef0d05d13
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2024-06-01 22:28:46 +0800

    folderView

---
+++ client/src/pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <folderView/>
+    </div>
+</template>
+
+<script>
+import folderView from '@/components/folderView.vue';
+export default {
+    name: 'Test',
+    components: {
+        folderView
+    }
+};
+</script>
