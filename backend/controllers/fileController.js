52dfe2f8d92be493c06a6dc3c8e1319be5953aa0
Johann Sebastian Catalla <catallajohann@gmail.com>
2024-06-02 02:12:01 +0800

    file controller

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -124,5 +124,6 @@ module.exports = {
     getFolders,
     getFiles,
     getFile,
+    addFile,
     createFolder
 };
--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -4,12 +4,8 @@ const {
     getFolders,
     getFiles,
     getFile,
-<<<<<<< HEAD
-    addFile
-=======
-
+    addFile,
     createFolder
->>>>>>> 949c26f718b0a948c63fa66411a5840950083405
 } = require("../controllers/fileController");
 
 const validateToken = require("../middleware/validateTokenHandler");
@@ -18,12 +14,9 @@ router.use(validateToken);
 router.route("/dashboard").get(getFolders);
 router.route("/dashboard/folder/:folder_name").get(getFiles);
 router.route("/dashboard/folder/:folder_name/:event_id").get(getFile);
-<<<<<<< HEAD
 router.route("/dashboard/folder/:folder_name").post(addFile);
     
-=======
 router.route("/dashboard").post(createFolder);
 
 
->>>>>>> 949c26f718b0a948c63fa66411a5840950083405
 module.exports = router;
